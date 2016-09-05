4fe05e84b33f050c711caaab2d1647d96adebedf
ejcox24@gmail.com <ejcox24@gmail.com>
2016-09-05 12:12:45 -0700

    correct boundaries
    fixed glitch that restarts turn when your entered number exceeds the
    boardgame boundary

--- battleship.py
+++ battleship.py
@@ -27,35 +27,58 @@ def newfield(level):
         for n in y: #nested for loop - eg a[i] where i is the empty list
             rand = random.choice([0,1]);
             a[i].append(rand) #keep adding
-    return a;
+    return a; #eg [[1,0],[0,0],[1,0]]
 
 a=newfield(level)
 e=countships(a);
-
+'''
+def drawfield(a):
+"""input will show direct hits, converts array in"""
+    z=[];
+    for i in a:
+        z+=([]);
+        for x in i:
+            z+=('X');
+    z[xattack][yattack]==1;
+    print(z);
+    return z;
+        '''
 
 def battleship(a,e):
     """ where inputs are x, the rows, y, the columns, and a the battlefield. Output is a string"""
-
+    if e==0:
+        print('all enemy ships destroyed. Congratulations captain!');
+        return;
     print('you have ' + str(e) + ' battleships left to destroy');
     
     xattack = int(input('choose an x attack coordinate between 0 and ' + str(len(a)-1))); #this is width (columns)
     yattack = int(input('choose an y attack coordinate between 0 and ' + str(len(a[0])-1))); #this is height (rows)
-    if (xattack>len(a)  or yattack>len(a[0])):
+    print('# of rows in array is ' + str(len(a)));
+    print('number you chose is ' + str(xattack));
+    if xattack>=len(a)  or yattack>=len(a[0]):
         print('oops! please stick to the boundaries!');
         battleship(a,e);
     else: 
-        if a[xattack][yattack]==1:
+        if a[xattack][yattack]==1: #1 means there's a ship there
             print('Yeeha! A direct hit!')
-            a[xattack][yattack]=0;
-            e-=1;
-            if e==0:
-                print('all enemy ships destroyed. Congratulations captain!');
-                return;
-            else:
-                battleship(a,e-1);
+            a[xattack][yattack]=2;
+            #draw graf here
+        
+            graf = '' #empty string will print out complete string 
+            for i in a: # eg [[1,0],[0,0],[1,0]] i is [1,0]
+                graf+='\n' #add new line here
+                for l in i:#the columns
+                    if l != 2: #where there's no direct hit add an 'X'
+                        graf+='X'
+                    else:
+                        graf+='O'
+            print (graf);
+            battleship(a,e-1);
         else:
             print('you missed! Try another location');
+            #and here
             battleship(a,e);
+            
 
 
 if play == 1:
